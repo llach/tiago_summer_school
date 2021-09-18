4a47637f76a0df69db4e367ff6c2faed3e6856c9
llach <lachluca@gmail.com>
2021-09-18 12:31:45 +0200

    add comments to source files

--- force_control.py
+++ force_control.py
@@ -6,18 +6,30 @@ from tiago_rl.misc import LoadCellVisualiser
 # Environment setup
 # ----------------------------
 
+"""
+we first create the simulation environment for the TIAGo sensorised gripper.
+it can either be position or velocity controller (whichever you prefer).
+"""
+
+POS_CTRL = 'pos'
+VEL_CTRL = 'vel'
+
+# whether to render the environment or not
 show_gui = True
-force_type = 'raw'
+
+# target force to reach and maintain. your controller should be able to reach different target forces, not just this one
 target_forces = np.array(2*[0.2])
 
-
 env = GripperTactileEnv(show_gui=show_gui, 
-                        force_type=force_type, 
-                        target_forces=target_forces)
+                        target_forces=target_forces,
+                        control_mode=VEL_CTRL)
 
 # Visualisation setup
 # ----------------------------
-
+"""
+additionally, you can visualize certain variables from the environment's state such as
+current joint positions, velocities, forces (with target force) and more.
+"""
 vis = None
 if show_gui:
     vis = LoadCellVisualiser(env)
@@ -25,20 +37,46 @@ if show_gui:
 # Control Loop
 # ----------------------------
 
-env.reset()
-for i in range(300):
-    # TODO do force control
+rewards = [] # we use the cumulative reward as a performance metric
+for i in range(500):
+    """
+    current_q and current_vel are dicts mapping joint names 
+    to the current joint position and velocity, respectively:
+    
+    as an example, the position dict would look like this for a fully open gripper:
+    current_q = {
+        'gripper_right_finger_joint': 0.045,
+        'gripper_left_finger_joint': 0.045
+    }
+    """
+    current_q, current_vel = env.get_state_dicts()
+
+    """
+    to get the current forces, we access the environment's member current_forces
+    """
+    current_forces = env.current_forces
+
+    """
+    as the action for the environment consists of a desired joint state for all joints, we can use
+    BulletRobotEnv.create_desired_state() to create one even if we only want to control a subset of joints.
+    
+    this is not as important for GripperTactileEnv, which only controls the two finger joints, but very
+    convenient for environments that simulate the whole robot
+    """
+    # TODO create desired state such that the target force is reached and maintained
     new_state = env.create_desired_state({
         'gripper_right_finger_joint': -0.2,
         'gripper_left_finger_joint': -0.2,
     })
 
+    """
+    next, we hand over the action (desired joint state) to the environment and receive 
+    observations, reward, done (bool) and additional info in return
+    """
     obs, reward, done, info = env.step(new_state)
+    rewards.append(reward) # store scalar reward r(t) in rewards list
 
-    # extract information from observations.
-    # see GripperTactileEnv._get_obs() for reference.
-    f = obs[-2:]
-
+    # finally, we update the plot
     if vis:
         vis.update_plot(is_success=info['is_success'], reward=reward)
-    
+print(f"final episode reward {np.sum(rewards)}")
--- learn_control.py
+++ learn_control.py
@@ -5,16 +5,39 @@ from tiago_rl.envs import GripperTactileEnv
 
 # Environment setup
 # ----------------------------
+"""
+the environment for training is created equally to the one in the first task 
+in order for them to be comparable.
 
-force_type = 'raw'
+we only add the TimeLimit wrapper so that the environment emits a done=True
+after N steps
+"""
+POS_CTRL = 'pos'
+VEL_CTRL = 'vel'
+
 target_forces = np.array(2*[0.2])
 
 env = GripperTactileEnv(show_gui=False, 
-                        force_type=force_type, 
-                        target_forces=target_forces)
-env = TimeLimit(env, max_episode_steps=300)
+                        target_forces=target_forces,
+                        control_mode=VEL_CTRL)
+env = TimeLimit(env, max_episode_steps=500)
 
 # RL Training setup
 # ----------------------------
+"""
+here, you are free to use whichever RL algorithm or package you prefer.
+in case you don't know where to start, we recommend having a look at the examples from stable-baselines3:
+https://stable-baselines3.readthedocs.io/en/master/guide/examples.html
 
-# TODO setup and train model
+generally, this script should:
+* create a model (make sure to use an algorithm that can handle continuous actions)
+* train the model
+* save model weights (usually only when new high-scores are achieved to save disk space)
+
+then, in another script, you can:
+* load the weights of the best performing model
+* evaluate the model by:
+    * watching its behavior in the environment
+    * comparing the cumulative reward with that of your hand-written force controller
+"""
+# TODO create and train model
